b386783cc20562d92c5fd219dfe6b079970f96fb
Christian Schleidt <cschleidt@hotmail.com>
2023-02-15 08:35:48 +0100

    changed start script

--- dist/server.js
+++ dist/server.js
@@ -9,6 +9,10 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+// Handling non matching request from the client
+app.use((req, res) => {
+    res.status(404).send("<h1>Page not found on the server</h1>");
+});
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
