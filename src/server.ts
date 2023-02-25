b3fb78a1f9c618c0940ca95e18d7df25547fdd56
Christian Schleidt <cschleidt@hotmail.com>
2023-02-25 08:06:38 +0100

    added app insights

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+import appInsights from 'applicationinsights'
+appInsights.setup("InstrumentationKey=844a6dc6-25b1-4e2a-b2ff-52fb82f19e2b;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/").start();
+
+
+
 import dotenv from "dotenv"
 import Express, { json } from 'express'
 import router from './router'
-//import cors from 'cors'
+import cors from 'cors'
 import morgan from 'morgan'
 
 
@@ -13,7 +18,7 @@ const PORT = process.env.PORT || 8000
 App.use(morgan('short'))
     .use(json())
     .use(router)
-//    .use(cors())
+    .use(cors())
 
 // Handling non matching request from the client
 App.use((req, res) => {
